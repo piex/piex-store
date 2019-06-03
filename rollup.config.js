da77fd82110043c44413ca93ce8d130a9042cc0e
foreverzmy <921255465@qq.com>
2019-06-03 20:21:54 +0800

    fix: package config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const rollup = require('rollup');
 const rollupTypescript = require('rollup-plugin-typescript');
-const { dts } = require("rollup-plugin-dts");
 
 const packages = fs.readdirSync('packages');
 
@@ -35,43 +34,6 @@ async function build(package) {
   await bundle.write(outputOptions);
 }
 
-async function buildDts(package) {
-  const input = `packages/${package}/src/index.ts`;
-
-  const inputOptions = {
-    input,
-    external: [
-      'lodash',
-      'react'
-    ],
-    plugins: [
-      dts({
-        tsconfig: fs.readFileSync('./tsconfig.json').toString()
-      })
-    ],
-    treeshake: { moduleSideEffects: false },
-    output: [{
-      file: `packages/${package}/lib/index.d.ts`,
-      format: 'es'
-    }]
-  };
-
-  const outputOptions = {
-    file: `packages/${package}/lib/index.d.ts`,
-    format: 'es'
-  };
-
-  // create a bundle
-  const bundle = await rollup.rollup(inputOptions);
-
-  // generate code and a sourcemap
-  await bundle.generate(outputOptions);
-
-  // or write the bundle to disk
-  await bundle.write(outputOptions);
-}
-
 packages.map(async (package) => {
   await build(package);
-  await buildDts(package);
 })
